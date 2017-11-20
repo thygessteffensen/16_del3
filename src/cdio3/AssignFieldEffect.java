32eca9a2af233417be48e28b3af4b18e301985d7
Mathias <mathias_1337@hotmail.com>
2017-11-20 11:48:35 +0100

    Oprettelse af AssignFieldEffect

---
+++ src/cdio3/AssignFieldEffect.java
@@ -0,0 +1,79 @@
+package cdio3;
+
+public class AssignFieldEffect {
+	switch(Player.getPlayerLocation) {
+	case 0: start {
+		Player.wallet.changeBalance += 2;
+		break;
+	}
+	case 1: {
+		
+		break;
+	}
+	case 2: {
+		
+		break;
+	}
+	case 3: case 9: case 15: case 21: {
+		ChanceKort;
+		break;
+	}
+	case 4: {
+		
+		break;
+	}
+	case 5: {
+		
+		break;
+	}
+	case 6: {
+		
+		break;
+	}
+	case 7: {
+		
+		break;
+	}
+	case 8: {
+		
+		break;
+	}
+	case 10: {
+		
+		break;
+	}
+	case 11: {
+		
+		break;
+	}
+	case 12: {
+		
+		break;
+	}
+	case 13: {
+		
+		break;
+	}
+	case 14: {
+		
+		break;
+	}
+	case 16: {
+		
+		break;
+	}
+	case 17: {
+		
+		break;
+	}
+	case 18: {
+		Jail
+		break;
+	}
+	case 19: {
+		
+	}
+}
+
+
+}
