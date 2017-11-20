49a2ad08e105b8daaee57a660d3b662d12510922
Mathias <mathias_1337@hotmail.com>
2017-11-20 10:57:19 +0100

    Ændring af terning klassen
    -Dice2

--- src/cdio3/Dice.java
+++ src/cdio3/Dice.java
@@ -4,21 +4,20 @@ import java.util.Random;
 
 public class Dice
 {
-	private int d1, d2; // Value of dice 1 and 2.
+	private int d1; // Value of dice 1 and 2.
 	
 	Random ran = new Random(); // Instance of the Random class.
 	
 	/**
 	 * Bruger Random klassen til at slå med en terning
-	 * @return Summen af terningerne
+	 * @return Summen af terningen
 	 */
 	public void roll()
 	{
 		//To terninger defineres.
 		d1 = ran.nextInt(6)+1;
-		d2 = ran.nextInt(6)+1;
 	}
-	
+
 	/**
 	 * getDiceValue1 returnerer værdien d1 
 	 * @return
@@ -26,22 +25,5 @@ public class Dice
 	public int getDiceValue1() {
 		return d1;
 	}
-	
-	/**
-	 * getDiceValue2 returnerer værdien d2  
-	 * @return
-	 */
-	public int getDiceValue2() {
-		return d2;
-	}
-	/**
-	 * getSum retunererer værdien af GetDiceValue 1 og getDiceValue2
-	 * @return
-	 */
-	public int getSum() {
-		return getDiceValue1() + getDiceValue2();
-	}
-	
-	
-	
+
 }
