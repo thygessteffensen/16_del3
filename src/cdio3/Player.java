29c42e2b7a47427011898480d09f9cf3397f8267
The-Hest <s175195@win.dtu.dk>
2017-11-21 16:40:25 +0100

    +Player
    Player klassen påbegyndt

--- src/cdio3/Player.java
+++ src/cdio3/Player.java
@@ -1,5 +1,39 @@
 package cdio3;
 
-public class Player {
-
+public class Player 
+{
+	private Brik brik = new Brik();
+	private int brikNum
+	private int alder; 
+	private Wallet wallet = new Wallet();
+	public int currentField = -1; 
+	
+	public Player() {
+	}
+	
+	/**
+	 * brik
+	 */
+	public Brik getBrik(){
+		return brik;
+	}
+	
+//	/**
+//	 * Alder (vis nødvendig)
+//	 */
+//	public Player (int alder) {
+//		this.alder = alder;
+//	}
+//	public int getAlder() {
+//		return alder;
+//	}
+	
+	/**
+	 * Wallet
+	 * @return
+	 */
+	public Wallet getWallet() {
+		return wallet;
+	}
+	
 }
