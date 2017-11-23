bffb141b8ff0766abe6a446098793a6cbf73eefb
thygessteffensen <31892312+thygessteffensen@users.noreply.github.com>
2017-11-23 14:39:14 +0100

    +main

--- src/cdio3/Main.java
+++ src/cdio3/Main.java
@@ -1,17 +1,20 @@
 package cdio3;
 
+import java.io.IOException;
+
 public class Main {
 	static UI ui;
 	static Player[] player; // Player array der indeholder referrencer til player objekter.
-	String [] optPiece = {reader.getString("skib", "brikker"), reader.getString("bil", "brikker")
-			,reader.getString("kat", "brikker"),reader.getString("hund", "brikker")};
-	}
+	static Reader reader = new Reader();
 	
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		ui = new UI();
 		ui.setUp();
-
+		String[] optPiece = {reader.getString("skib", "brikker"), reader.getString("bil", "brikker")
+				,reader.getString("kat", "brikker"),reader.getString("hund", "brikker")};
+		
 		String[] optPlayer = {"2", "3", "4"};
 		int playerCount;
 		//		playerCount = Integer.parseInt(ui.dropDown(reader.getString("spillerAntal", "velkommen"), {"2", "3", "4"));
@@ -20,20 +23,22 @@ public class Main {
 		ui.getUserResponse("startPenge"+(2-1), "OK!");
 		player = new Player[playerCount];
 		
-		
+		String taken = null;
 		for (int i = 0; i < player.length ; i++) {
-			availiblePieces() = optPiece;
+			optPiece = availiblePieces(taken, optPiece);
 			player[i].setPiece(ui.dropDown(reader.getString("vælgBrik","velkommen"), optPiece));
 		}
 
 
 	}
-	public String[] availiblePieces(String taken, String[] opt) {
+	public static String[] availiblePieces(String taken, String[] opt) {
 		String[] ap = new String[opt.length];
+		String temp = "";
 		for (int i = 0; i < opt.length; i++) {
 			if(!taken.equals(opt[i])) {
-				
+				temp += opt[i] + " ";
 			}
+			
 			
 			
 		}
