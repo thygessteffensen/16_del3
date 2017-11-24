fdff483ee0d592ea5201cc3a1e445827ed55762b
Mathias <mathias_1337@hotmail.com>
2017-11-24 09:37:18 +0100

    +Ændrede wallet til up-to-date

--- cdio3/Wallet.java
+++ cdio3/Wallet.java
@@ -1,25 +1,52 @@
 package cdio3;
 
-public class Wallet {
-	super switch (Playercount) {
-	case 2: balance = 20; break;
-	case 3: balance = 18; break;
-	case 4: balance = 16; break;
-	default: System.out.println("Fejl i playercount");
-	}
-
-
+public class Wallet extends Player{
 	
-	public getBalance() { // Getter til balance
+	private int balance;
+	private boolean playerLost = false;
+	
+	/**
+	 * Konstruktør
+	 * @param piece
+	 * @param playerCount
+	 * @param balance
+	 */
+	public Wallet(String piece, int playerCount, int balance) {
+		super(piece, playerCount);
+		this.balance = balance;
+	}
+	/**
+	 * Laver en getbalance så programmet ikke har direkte adgang til balance
+	 * @return
+	 */
+	public int getBalance() { // Getter til balance
+		return balance;
+	}
+	
+	/**
+	 * Laver en set balance så programmet kan hardset en balance på en player
+	 * @param value given værdi der gør metoden i stand til at hardset balancen
+	 */
+	public void setBalance(int value) {
+		balance += value;
 		
-		return balance
 	}
 	
-	public void setBalance(int balance) { // Setter til balance
+ 	/**
+ 	 * Tjekker om en spiller har tabt ved at se om balancen er under 0
+ 	 * @return
+ 	 */
+	public boolean playerLost() {
 		if (balance < 0)
-			PlayerLost; // Spiller har tabt
+			playerLost = true;
+		return playerLost;
 	}
-	public void changeBalance(int changebalance) { //Changebalance
-		changeBalance += changebalance;
+	
+	/**
+	 * Gør programmet i stand til at ændre en balance uden at skulle hardset den.
+	 * @param
+	 */
+	public void changeBalance(int changebalance) {
+		this.balance += changebalance;
 	}
 }
