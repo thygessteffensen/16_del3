44f334e4ac8a7230a2db67942a10e68d50375245
Mathias <mathias_1337@hotmail.com>
2017-11-24 09:35:45 +0100

    +Fixede set balance
    +Kommentar til javadoc

--- src/cdio3/Wallet.java
+++ src/cdio3/Wallet.java
@@ -1,34 +1,52 @@
 package cdio3;
 
 public class Wallet extends Player{
-
-
+	
 	private int balance;
 	private boolean playerLost = false;
-
+	
+	/**
+	 * Konstruktør
+	 * @param piece
+	 * @param playerCount
+	 * @param balance
+	 */
 	public Wallet(String piece, int playerCount, int balance) {
 		super(piece, playerCount);
 		this.balance = balance;
 	}
-
+	/**
+	 * Laver en getbalance så programmet ikke har direkte adgang til balance
+	 * @return
+	 */
 	public int getBalance() { // Getter til balance
 		return balance;
 	}
-	/* Sætter ikke balance, men checker derimod om en spillers konto er under 0
-	 * Enten skal metode navnet ændres til CheckPlayerLost ellers så skal den ændres til
-	 * at sætte en spillers konto til parameteren.
+	
+	/**
+	 * Laver en set balance så programmet kan hardset en balance på en player
+	 * @param value given værdi der gør metoden i stand til at hardset balancen
 	 */
-	public void setBalance(int balance) {
-		if (balance < 0)
-			playerLost = true;
+	public void setBalance(int value) {
+		balance += value;
 		
 	}
 	
-	public boolean getPlayerLost() {
-		return playerLost; // Spiller har tabt
+ 	/**
+ 	 * Tjekker om en spiller har tabt ved at se om balancen er under 0
+ 	 * @return
+ 	 */
+	public boolean playerLost() {
+		if (balance < 0)
+			playerLost = true;
+		return playerLost;
 	}
 	
-	public void changeBalance(int changebalance) { //Changebalance
+	/**
+	 * Gør programmet i stand til at ændre en balance uden at skulle hardset den.
+	 * @param
+	 */
+	public void changeBalance(int changebalance) {
 		this.balance += changebalance;
 	}
 }
