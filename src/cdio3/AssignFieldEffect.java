c6f565c5b6e8b2999862e1a8cad41e9dbff2537b
thygessteffensen <31892312+thygessteffensen@users.noreply.github.com>
2017-11-27 16:54:16 +0100

    +properties

--- src/cdio3/AssignFieldEffect.java
+++ src/cdio3/AssignFieldEffect.java
@@ -13,14 +13,18 @@ public class AssignFieldEffect {
 	boolean jail;
 	String message;
 	int newLocation;
-	
+	Properties properties;
+	int reciever;
+	int payer;
+	int amount;
+
 
 	/**
 	 * Initialisere felt effekten. 
 	 * Giver de respektive variabler, de rigitge værdier. 
 	 * @param location Lokationen på spilleren.
 	 */
-	public void initFieldEffect(int location) {
+	public void initFieldEffect(int location, int playerNumber) {
 		reset(location);
 		// switch der håndterer hvilke metode der skal benyttes, i forhold til hvilket felt man lander på.
 		switch(location) {
@@ -29,18 +33,32 @@ public class AssignFieldEffect {
 			break;
 		case 1: case 2: case 4: case 5:
 			propertyValue = 1;
+			properties.getEffect(location, propertyValue, playerNumber);
+			balance = properties.getRecieveAmount();
+			break;
+		case 6: case 12:
+			//TOM
 			break;
 		case 7: case 8: case 10: case 11:
 			propertyValue = 2;
+			properties.getEffect(location, propertyValue, playerNumber);
+			balance = properties.getRecieveAmount();
 			break;
+
 		case 13: case 14: case 16: case 17:
 			propertyValue = 3;
+			properties.getEffect(location, propertyValue, playerNumber);
+			balance = properties.getRecieveAmount();
 			break;
 		case 19: case 20:
 			propertyValue = 4;
+			properties.getEffect(location, propertyValue, playerNumber);
+			balance = properties.getRecieveAmount();
 			break;
 		case 22: case 23:
 			propertyValue = 5;
+			properties.getEffect(location, propertyValue, playerNumber);
+			balance = properties.getRecieveAmount();
 			break;
 		case 3: case 9: case 15: case 21:
 			cc.initChanceCard(location);
@@ -51,8 +69,9 @@ public class AssignFieldEffect {
 			jail = true;
 		default: break;
 		}
+		
 	}
-	
+
 	/**
 	 * Nulstiller alle variabler.
 	 * @param location Lokationen på spilleren.
@@ -64,26 +83,32 @@ public class AssignFieldEffect {
 		newLocation = location;
 		jail = false;
 	}
-	
-//----------------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------------
 	//                              getters
-//----------------------------------------------------------------------------------
-	 
+	//----------------------------------------------------------------------------------
+
 	public int getBalance() {
 		return balance;
 	}
-	
+
 	public int getNewLocation() {
 		return newLocation;
 	}
-	
+
 	public boolean getJail() {
 		return jail;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
 	
-
+	public int getPayer() {
+		return properties.getPayer();
+	}
+	
+	public int getPayAmount() {
+		return properties.getPayAmount();
+	}
 }
---
+++ src/cdio3/Properties.java
@@ -0,0 +1,101 @@
+package cdio3;
+
+/**
+ * 
+ * @author thyge
+ * @version 27.11.2017
+ */
+public class Properties {
+	// Nul baseret!
+	boolean[] owned;
+	int[] owner;
+	int fields = 24;
+	int propValue;
+	int playerNumber;
+	int location;
+	String message;
+	//------------
+	int reciever;
+	int payer;
+	int amount;
+
+	/**
+	 * Konstruktør
+	 */
+	public Properties() {
+		// generer arrays 
+		for (int i = 1; i < fields; i++) {
+			owned = new boolean[fields];
+			owner = new int[fields];
+			if((i % 3) != 0)
+				owned[i] = false;
+		}
+	}
+
+
+	/**
+	 * Finder ud af hvad der skal ske
+	 * @param location Lokationen hvor spilleren befinder sig. 0-baseret
+	 * @param propValue Værdien grunden har.
+	 * @param playerNumber Spillerens array index nummer
+	 */
+	public void getEffect(int location, int propValue, int playerNumber) {
+		this.propValue = propValue;
+		this.location = location;
+		this.playerNumber = playerNumber;
+		if(owned[location] == false) {
+			buy();
+		}
+		else if(owned[location] == true) {
+			pay();
+		}
+		else {
+			System.out.println("You shouldn't be here!");
+		}
+	}
+
+	/**
+	 * Håndterer scenariet hvor grunden er ejet.
+	 */
+	private void pay() {
+		payer = playerNumber;
+		reciever = owner[location];
+		amount = propValue;
+	}
+
+	/**
+	 * Håndterer scenariet hvor grunden skal købes
+	 */
+	private void buy() {
+		owner[location] = playerNumber;
+		amount = propValue;
+		reciever = owner[location];
+	}
+
+	/**
+	 * Returnerer modtageren.
+	 * @return Modtagerens plads i player arrayet.
+	 */
+	
+	public int getPayer() {
+		return payer;
+	}
+
+	/**
+	 * Returnerer beløbet +-
+	 * @return Beløbet er en int der både kan være negativ og positiv.
+	 */
+	public int getPayAmount() {
+		return -1 * amount;
+	}
+	
+	public int getRecieveAmount() {
+		return amount;
+	}
+	
+
+
+
+
+
+}
