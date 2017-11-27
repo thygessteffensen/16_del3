e521acde9621c2d305f9716555c60de4069eb628
Mathias <mathias_1337@hotmail.com>
2017-11-27 11:42:01 +0100

    Simon og Fager
    +jail metode

--- src/cdio3/Player.java
+++ src/cdio3/Player.java
@@ -1,34 +1,80 @@
 package cdio3;
 
+/**
+ * 
+ * @author Simon
+ *
+ */
 public class Player 
 {
-	private String piece
-	private Wallet wallet = new Wallet();
+	Dice dice = new Dice();
+	private String piece;
+	Wallet wallet;
+	int balance;
+	int location;
 
-	public Player(String piece) {
-
-		/**
-		 * brik
-		 */
-		public piece getPiece(){
-			return piece;
+	public Player(String piece, int playerCount) {
+		switch (playerCount) {
+		case 2: balance = 20;
+		break;
+		case 3: balance = 18;
+		break;
+		case 4: balance = 16;
+		break;
+		default: System.out.println("Fejl i wallet");
+		break;
 		}
-
-		/**
-		 * 
-		 * @return
-		 */
-		public void setPiece(String piece){
-			setPiece = piece;
-		}	
-
-		/**
-		 * Wallet
-		 * @return
-		 */
-		public Wallet getWallet() {
-			return wallet;
-		}
-
+		this.piece = piece;
 	}
+	
+	/**
+	 * Jail metode
+	 */
+	public void jail() {
+		if(location == 18) {
+			if(wallet.getBalance() <=3) {
+				if(<escapeBotton> == true) {
+					wallet.changeBalance(-2);
+					dice.roll();
+				}							
+			}
+			else dice.setDiceValue1(0);
+		}
+	}
+	
+	/**
+	 *  getLocation
+	 * @return location
+	 */
+	public int getLocation() {
+		return location;
+		
+	}
+	
+	
+	/**
+	 * getPiece
+	 * @return piece
+	 */
+	public String getPiece(){
+		return piece;
+	}
+
+	/**
+	 * setLocation
+	 */
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	
+	/**
+	 * setPiece
+	 */
+	public void setPiece(String piece){
+		this.piece = piece;
+	}	
+
+
+
 }
