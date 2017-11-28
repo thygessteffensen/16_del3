fc3ea85105081d3193a6aad255a4c35b30f8a0ae
Mathias <mathias_1337@hotmail.com>
2017-11-28 21:36:22 +0100

    +Opdaterede til nyeste player

--- src/cdio3/Player.java
+++ src/cdio3/Player.java
@@ -1,10 +1,17 @@
 package cdio3;
 
+/**
+ * 
+ * @author Simon og Mathias
+ *
+ */
 public class Player 
 {
+	Dice dice = new Dice();
 	private String piece;
 	Wallet wallet;
 	int balance;
+	int location;
 
 	public Player(String piece, int playerCount) {
 		switch (playerCount) {
@@ -20,17 +27,49 @@ public class Player
 		this.piece = piece;
 	}
 	
+	/**
+	 * Jail metode
+	 */
+	public void jail() {
+		if(location == 18) {
+			if(wallet.getBalance() <=3) {
+				if(<escapeBotton> == true) {
+					wallet.changeBalance(-2);
+					dice.roll();
+				}							
+			}
+			else dice.setDiceValue1(0);
+		}
+	}
 	
 	/**
-	 * brik
+	 *  getLocation
+	 * @return location
+	 */
+	public int getLocation() {
+		return location;
+		
+	}
+	
+	
+	/**
+	 * getPiece
+	 * @return piece
 	 */
 	public String getPiece(){
 		return piece;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * setLocation
+	 */
+	public void setLocation(int location) {
+		this.location = location;
+	}
+	
+	
+	/**
+	 * setPiece
 	 */
 	public void setPiece(String piece){
 		this.piece = piece;
