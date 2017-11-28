3e1ced35493b3b5ed44586730126dfab0e41f652
thygessteffensen <31892312+thygessteffensen@users.noreply.github.com>
2017-11-28 10:10:35 +0100

    + main

--- src/cdio3/Main.java
+++ src/cdio3/Main.java
@@ -14,64 +14,76 @@ public class Main {
 	static Reader reader = new Reader();
 	static Dice dice;
 	static AssignFieldEffect afe;
-	
-	
+	static int[] location;
+
+
 	public static void main(String[] args) throws IOException {
 		ui = new UI();
-		ui.setUp();
 		String[] optPiece = {reader.getString("skib", "brikker"), reader.getString("bil", "brikker")
 				,reader.getString("kat", "brikker"),reader.getString("hund", "brikker")};
-		
+
 		String[] optPlayer = {"2", "3", "4"};
 		int playerCount;
 		playerCount = Integer.parseInt(ui.dropDown(reader.getString("spillerAntal", "velkommen"), optPlayer));
 		ui.getUserResponse(reader.getString("startPenge"+(playerCount-1), "velkommen"), "OK!");
 		player = new Player[playerCount];
-		
-//--------------------------------------------------------------------------------------------------------------------
-//                                              Spillere tilføjes
-//--------------------------------------------------------------------------------------------------------------------
-	
+
+		//--------------------------------------------------------------------------------------------------------------------
+		//                                              Spillere tilføjes
+		//--------------------------------------------------------------------------------------------------------------------
+
 		/**
 		 * Tilføjer spiller med brik og så'n
 		 */
 		String taken = "none";
-		for (int i = 0; i < player.length ; i++) {
+		location = new int[playerCount];
+		for (int i = 0; i < playerCount ; i++) {
 			optPiece = availiblePieces(taken, optPiece);
 			taken = ui.dropDown(reader.getString("vælgBrik","velkommen"), optPiece);
 			player[i] = new Player(taken, playerCount);
 			player[i].wallet = new Wallet(taken, playerCount, 20);
 			ui.addPlayer(playerCount, i, player[i].getPiece(), player[i].wallet.getBalance());
+			ui.setUp(i);
+			location[i] = 0;
 		}
-		
-//--------------------------------------------------------------------------------------------------------------------
-//                                              Spillet påbegyndes:
-//--------------------------------------------------------------------------------------------------------------------
 
-//		for (int i = 0; i < optPlayer.length; i++) {
-//			while(player[i].wallet.getPlayerLost()) {
-//				// Tilføj gameplay her:
-//			}
-//		}
+		//--------------------------------------------------------------------------------------------------------------------
+		//                                              Spillet påbegyndes:
+		//--------------------------------------------------------------------------------------------------------------------
+
+		//		for (int i = 0; i < optPlayer.length; i++) {
+		//			while(player[i].wallet.getPlayerLost()) {
+		//				// Tilføj gameplay her:
+		//			}
+		//		}
+
 		
-		
-		while(!player[1].wallet.getPlayerLost()) {
-			ui.getUserResponse("Du må slå", "ok");
-			// Fix static
-			dice.roll();
-			ui.setDice(dice.getDiceValue1());
-			ui.setLocation();
+
+
+		int diceValue;
+		int oldLoc;
+		int j = 0;
+//		while(!player[j].wallet.getPlayerLost()) {
+		while(true) {
+			for (j = 0; j < playerCount; j++) {
+				System.out.println(player[j].getPiece());
+				ui.getUserResponse("Du må slå", "ok");
+				// Fix static
+				dice.roll();
+				diceValue = dice.getDiceValue1();
+				ui.setDice(diceValue);
+				oldLoc = location[j];
+				location[j] += diceValue;
+				if(location[j] >=24 )
+					location[j] = location[j] - 24;
+				ui.setLocation(j, oldLoc, location[j]);
+			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
 
 	}
 	public static String[] availiblePieces(String taken, String[] opt) {
--- src/cdio3/UI.java
+++ src/cdio3/UI.java
@@ -3,6 +3,7 @@ package cdio3;
 import gui_main.GUI;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
+import gui_fields.GUI_Street;
 
 // Link til Jdoc
 // http://htmlpreview.github.io/?https://raw.githubusercontent.com/diplomit-dtu/Matador_GUI/repository/Desktop_GUI/doc/index.html
@@ -19,14 +20,30 @@ public class UI {
 	GUI_Player[] player;
 
 
+	public UI() {
+		fields = getFields();
+		gui = new GUI(fields);
+	}
+
+	public GUI_Field[] getFields() {
+		GUI_Field[] temp = new GUI_Field[24];
+
+		temp[0] = new GUI_Street();
+
+		for (int i = 1; i < temp.length; i++) {
+			temp[i] = new GUI_Street();
+			temp[i].setTitle("Brik " + i);
+		}
+		return temp;
+	}
+
 	/**
 	 * Opsætter spillepladen ved init.
 	 */
-	public void setUp() {
-		gui = new GUI();
-		fields = gui.getFields();
+	public void setUp(int playerNumber) {
+		fields[0].setCar(player[playerNumber], true);	
 	}
-	
+
 	/**
 	 * Tilføjer spillere til GUI_Player, såvel som på pladen
 	 * @param playerCount Antal spillere
@@ -38,9 +55,8 @@ public class UI {
 		player = new GUI_Player[playerCount];
 		player[i] = new GUI_Player(name, balance);
 		gui.addPlayer(player[i]);
-	
 	}
-	
+
 	/**
 	 * Sætter terninger med de respektive antal højen på spille pladen.
 	 * @param d1 Antal øjne på terning 1
@@ -49,7 +65,7 @@ public class UI {
 	public void setDice(int d1) {
 		gui.setDice(d1, 1);
 	}
-	
+
 	/**
 	 * Danner en dropdownmenu, hvor i man kan vælge mellem strengene
 	 * i Streng arrayet. Der retuneres en streg
@@ -60,7 +76,7 @@ public class UI {
 	public String dropDown(String Message, String[] opt) {
 		return gui.getUserSelection(Message, opt);
 	}
-	
+
 	/**
 	 * Modtag bruger respons
 	 * @param msg Besked til spilleren
@@ -69,7 +85,7 @@ public class UI {
 	public void getUserResponse(String msg, String button) {
 		gui.getUserButtonPressed(msg, button);
 	}
-	
+
 	/**
 	 * Retunerer en string som bruger giver os, ud fra beskden msg
 	 * @param msg Besked der skal vises til spilleren.
@@ -78,9 +94,10 @@ public class UI {
 	public String getUserString(String msg) {
 		return gui.getUserString(msg);
 	}
-	
-	public void setLocation() {
-		fields[0].setCar(player[0], true);
-	}
 
+	public void setLocation(int playerNumber, int oldLocation, int newLocation) {
+		System.out.println(player[playerNumber].toString());
+		fields[oldLocation].setCar(player[playerNumber], false);
+		fields[newLocation].setCar(player[playerNumber], true);
+	}
 }
