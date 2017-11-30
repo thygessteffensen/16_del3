c6ff5f6d2fa50ac0cdf23d4e20d7ba5bb344ba54
thygessteffensen <31892312+thygessteffensen@users.noreply.github.com>
2017-11-30 22:23:13 +0100

    Dice

--- src/cdio3/Dice.java
+++ src/cdio3/Dice.java
@@ -3,19 +3,17 @@ package cdio3;
 import java.util.Random;
 
 /**
- * 
- * @author Mathias
+ * @author CDIO2
  * @version 28/11/2017
  */
 
 public class Dice
 {
 	private static int d1; // Value of dice 1 and 2.
-	
 	static Random ran = new Random(); // Instance of the Random class.
 	
 	/**
-	 * Bruger Random klassen til at slå med en terning
+	 * Bruger Random klassen til at genererer et tilfældigt tal.
 	 * @return Summen af terningen
 	 */
 	public static void roll()
